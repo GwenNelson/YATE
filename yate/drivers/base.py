7b0d1b8c35b514000114e557345a8277cda8cb42
Gareth Nelson <gareth@garethnelson.com>
2016-12-13 19:04:36 +0000

    flesh out more of the mock driver

--- yate/drivers/base.py
+++ yate/drivers/base.py
@@ -4,16 +4,50 @@ from yateproto import *
 
 class YateBaseVoxel:
 """ This class may either be used directly or inherited from and extended for game-specific mechanics etc
+    Instances of this class should be replaced completely, not edited, when the environment changes
+    Basically, this class should always be read only
 """
-   def __init__(self,spatial_pos=(64,64,64),basic_type=YATE_VOXEL_EMPTY,specific_type=0):
+   def __init__(self,spatial_pos=(64,64,64),basic_type=YATE_VOXEL_EMPTY,specific_type=0,active_state=YATE_VOXEL_INACTIVE,intact_state=YATE_VOXEL_INTACT):
        """ spatial_pos   is obvious
            basic_type    is the basic voxel type as defined in yateproto.py
            specific_type is optional and specifies the game-specific extended type
+           active_state and intact_state only make sense in certain contexts:
+               doors are active when open, other stuff may be active or not if it makes sense
+               partly destroyed voxels are not intact
        """
        self.spatial_pos   = spatial_pos
        self.basic_type    = basic_type
        self.specific_type = specific_type
-
+       self.active_state  = active_state
+       self.intact_state  = intact_state
+   def is_intact(self):
+       """" return a boolean value indicating whether or not this voxel is fully intact
+            if it's partly destroyed, this will return false
+       """
+       if self.intact_state==YATE_VOXEL_INTACT: return True
+       return False
+   def is_active(self):
+       """ return a boolean value indicating whether or not this voxel is active, whatever that means in context
+       """
+       if self.intact_state == YATE_VOXEL_ACTIVE: return True
+       return False
+   def is_open(self):
+       """ return a boolean value indicating whether or not this voxel is open - this makes no sense unless it's a door
+           none-door voxels can thus never be opened
+       """
+       return self.can_open() and self.is_active()
+   def can_open(self):
+       """ return a boolean value indicating whether or not this voxel can be opened, i.e is it a door
+           this method could be overridden to implement keys and such
+       """
+       return {YATE_VOXEL_EMPTY:             False,
+               YATE_VOXEL_TOTAL_OBSTACLE:    False,
+               YATE_VOXEL_EASY_OBSTACLE:     False,
+               YATE_VOXEL_DOOR_OBSTACLE:     True,
+               YATE_VOXEL_DOOR_EASY_DESTROY: self.is_intact(), # we assume doors that are not intact can not be opened
+               YATE_VOXEL_DOOR_HARD_DESTROY: self.is_intact(),
+               YATE_VOXEL_HARD_OBSTACLE:     False,
+               YATE_VOXEL_UNKNOWN:           False}[self.basic_type]       
    def can_traverse(self, no_destroy=False, no_interact=False):
        """ return a boolean value indicating whether or not this voxel can be traversed by YATE automatically during pathfinding
            if no_destroy is set to True, we assume that YATE_VOXEL_EASY_OBSTACLE will not be destroyed
@@ -21,6 +55,7 @@ class YateBaseVoxel:
            if the voxel basic type is unknown, we assume it can NOT be traversed until proven otherwise
            this method will also throw an exception if self.basic_type is not a proper basic type
        """
+       if self.isopen: return True # this only makes sense for doors - if we are not a door, isopen should always be False
        return {YATE_VOXEL_EMPTY:             True,
                YATE_VOXEL_TOTAL_OBSTACLE:    False,
                YATE_VOXEL_EASY_OBSTACLE:     not no_destroy,
@@ -53,6 +88,11 @@ class YateBaseDriver:
            if doing so is appropriate. If the setup fails, then the constructor should throw an exception
        """
        pass
+   def get_vision_range(self):
+       """ Returns an (x,y,z) tuple representing how many voxels can be perceived by the in-game avatar
+           z is height
+       """
+       pass
    def get_msg_handlers(self):
        """ Return a dictionary of message types mapped to message handlers that the driver wants to deal with directly
        """
@@ -73,18 +113,33 @@ class YateBaseDriver:
        """ This method should return a tuple representing coordinates in 3D space of the AI's avatar
        """
        pass
-   def walk_to_space(self,x,y,z):
-       """ This method should attempt to walk to the specified space with an appropriate pathfinding algorithm
+   def destroy_voxel(self,spatial_pos):
+       """ This method attempts to destroy the voxel specified
+       """
+       pass
+   def interact_voxel(self,spatial_pos):
+       """ This method attempts to interact with the voxel specified
+       """
+       pass
+   def walk_to_space(self,spatial_pos):
+       """ This method should attempt to walk as close as possible to the specified space with an appropriate pathfinding algorithm
            Failed attempts are acceptable as this method is intended as a primitive for higher-level algorithms on the AI side
        """
        pass
-   def get_voxel(self,x,y,z):
+   def move_vector(self,vector):
+       """ This method is used by the base pathfinding algorithm to move the avatar in a specific direction
+           vector is simply a tuple specifying a 3D vector (i.e how much to try and move in each direction)
+           As with walk_to_space() failed attempts are both acceptable and expected
+           This method should attempt to destroy and interact as appropriate
+       """
+       pass
+   def get_voxel(self,spatial_pos):
        """ This method should return a voxel object describing the specified 3D coordinates. If the AI's avatar can not
            see the coordinates this method should return None
        """
        pass
    def get_voxel_strings(self):
-       """ This method should return a dict that maps voxel type integers to strings
+       """ This method should return a dict that maps extended voxel type integers to strings
        """
        pass
    def get_entity_strings(self):
--- yate/drivers/mockdriver.py
+++ yate/drivers/mockdriver.py
@@ -1,16 +1,82 @@
 """ This file is an example driver for implementing other games
+    It implements a static 2D environment with walls around the edges and some obstacles in the middle to test pathfinding
+    Visual range is set to the size of the whole map, which is a bit of a cheat
+    The map is surrounded by a void of unknown voxels
+    Turning this thing into a raycaster maze that runs on its own would not be too difficult
 """
 import base
+
+mock_environment = """
+77777777777777777777777777777777
+77711111111111111111111111777777
+77710010000000030000000001777777
+77710010111111114111111111777777
+77710010100000010000000001777777
+77710010100000010000000001777777
+77710010100000010000000001777777
+77710010100000000000000001777777
+777100121111110000A0000001777777
+77710000000000000000000001777777
+77711111111111111111111111777777
+77777777777777777777777777777777
+77777777777777777777777777777777
+"""
+
+def create_env():
+    """ Create a hashmap of the mock environment and return it along with avatar coordinates and visual range
+        Return format is a tuple (env,avatar_pos,visual_range)
+    """
+    env = {}
+    x = 0
+    y = 0
+    z = 0 # because it's 2D, everything is on a single plane
+    avatar_pos = None
+    for line in mock_environment.split('\n'):
+        x += 1
+        for c in line:
+            y += 1
+            if c=='A':
+               avatar_pos = (x,y,z)
+               b_type = 0
+            else:
+               b_type = int(str(c))
+            env[(x,y,z)] = base.YateBaseVoxel(spatial_pos=(x,y,z),basic_type=b_type)
+    return (env,avatar_pos,(x,y,1))
 
 class MockDriver(base.YateBaseDriver):
    def __init__(self,username=None,password=None,server=None):
        self.username    = username
        self.password    = password
        self.server_addr = server
-       self.spatial_pos = (64,64,64)
+       env,avatar_pos,visual_range = create_env()
+       self.env          = env
+       self.spatial_pos  = avatar_pos
+       self.visual_range = visual_range
    def get_mypos(self):
        return self.spatial_pos
-   def walk_to_space(self,x,y,z):
-       self.spatial_pos = (x,y,z)
+   def move_vector(self,vector):
+       """ This function squashes the vector into 2D space, scales it so it only moves 1 voxel at a time and
+           finally moves in the relevant direction if possible
+       """
+       cur_x,cur_y,cur_z = self.get_mypos()
+       vec_x,vec_y,vec_z = int(vector[0]),int(vector[1]),int(vector[2])
+       if vec_x > 0: vec_x = 1
+       if vec_y > 0: vec_y = 1
+       if vec_x < 0: vec_x = -1
+       if vec_y < 0: vec_y = -1
+       new_x = cur_x + vec_x
+       new_y = cur_y + vec_y
+       new_z = cur_z
+       new_vox = self.get_voxel((new_x,new_y,new_z))
+       if new_vox.can_traverse():
+          if new_vox.can_destroy():
+             self.destroy_voxel(new_vox)
+          else:
+             if new_vox.can_open():
+                self.interact_voxel(new_vox)
+          self.spatial_pos = new_vox
+
+   def get_voxel(self,spatial_pos):
+       return self.env[tuple(spatial_pos)]
 
 driver = MockDriver()
